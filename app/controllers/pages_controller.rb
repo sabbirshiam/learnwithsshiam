ed092358107e055cfa61859145e1cb08125bf3c0
sabbirshiam <sabbirshiam@gmail.com>
2019-08-25 12:11:51 +0900

    remove not working bootstrap css

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+	def index
+	end
+	def new
+	end
+	def create
+	end
+	def update
+	end
+	def edit
+	end
+	def show
+	end
+	def destroy
+	end
+
+	def about
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-	get "/", to: "homepage#home", as: "root"
+	get "/", to: "pages#index", as: "root"
   	get 'homepage/home'
+
+  	resources 'pages'
+  	get 'about', to: 'pages#about'
   	# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
